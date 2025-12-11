5b20a4c1589cb7474748c77dd760a46804741c7f
SaliraDaxius <saliradaxius@proton.me>
2025-12-10 23:56:50 -0500

    Installed hyprland

---
+++ common/hyprland.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }: {
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true; # recommended for most users
+    xwayland.enable = true; # Xwayland can be disabled.
+  };
+}
