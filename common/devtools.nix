39d111e499022bf2a94f179bb1531b0101b0682e
SaliraDaxius <saliradaxius@proton.me>
2025-12-13 21:48:03 -0500

    Tried to add VSCodium and IntelliJ IDEA CE

---
+++ common/devtools.nix
@@ -0,0 +1,16 @@
+{ pkgs, config, ... }: {
+  users.users.salira.packages = with pkgs; [
+    jetbrains.idea-community
+  ];
+
+  home-manager = {
+    users.salira = {
+      programs = {
+        vscode = {
+	  enable = true;
+	  package = pkgs.vscodium;
+	};
+      };
+    };
+  };
+}
