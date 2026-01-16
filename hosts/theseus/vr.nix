4a3602dbdab1af91bc6f407cd6f1594b942d3d18
SaliraDaxius <saliradaxius@proton.me>
2026-01-16 00:59:59 -0500

    More VR tweaks

--- hosts/theseus/vr.nix
+++ hosts/theseus/vr.nix
@@ -55,14 +55,14 @@ in
       STEAMVR_LH_ENABLE = "true";
       XRT_COMPOSITOR_COMPUTE = "1";
       XRT_COMPOSITOR_SCALE_PERCENTAGE = "200";
-      XRT_COMPOSITOR_DESIRED_MODE = "1";
+      XRT_COMPOSITOR_DESIRED_MODE = "0";
       # 0: 2880x1600@90.00
       # 1: 2880x1600@80.00
       # 2: 2880x1600@120.02
       # 3: 2880x1600@144.00
       XRT_COMPOSITOR_USE_PRESENT_WAIT = "1"; # NVIDIA "fix" for stuttering -> https://forums.developer.nvidia.com/t/substantial-drm-lease-presentation-latency-resulting-in-unusable-vr-hmd-experience/332386
       U_PACING_COMP_TIME_FRACTION_PERCENT = "90"; # NVIDIA "fix" for stuttering -> https://forums.developer.nvidia.com/t/substantial-drm-lease-presentation-latency-resulting-in-unusable-vr-hmd-experience/332386
-      U_PACING_COMP_MIN_TIME_MS = "12"; # Potential fix for view stuttering, if needed -> https://lvra.gitlab.io/docs/distros/nixos/#monado
+      U_PACING_COMP_MIN_TIME_MS = "15"; # Potential fix for view stuttering, if needed -> https://lvra.gitlab.io/docs/distros/nixos/#monado
 
       # XRT_COMPOSITOR_PRINT_MODES = "1";
       # XRT_COMPOSITOR_LOG = "debug";
